8aaa065e9022d7ab4b8a7b9817e53f24245c9ce9
Mattias Rensmo <mattias@rensmo.se>
2024-04-16 15:23:39 +0200

    nurå

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,21 +1,41 @@
-const Datastore = require('nedb-promise')
-const db = new Datastore({ filename: './database/database.db', autoload: true })
+const nedb = require('nedb-promise')
+const db = new nedb({ filename: 'database.db', autoload: true })
+const moment = require('moment')
+moment.locale('sv')
 
-class Order {
-  static async createOrder(orderDetails) {
-    const order = {
-      // schema
-      type: 'order',
-      price: orderDetails.price,
-      customer: orderDetails.items,
-      items: orderDetails.items,
+const GetOrderByID = async id => {
+  const dbEntry = await db.findOne({ _id: id })
+  return dbEntry
+}
+
+const CreateOrder = async content => {
+  dbRes = await db.insert(content)
+  return dbRes
+}
+
+const getOrderHistory = async userID => {
+  try {
+    const orderHistory = await db.find(
+      { type: 'order', customer: userID },
+      { orderDate: 1, price: 1, _id: 0 }
+    )
+    console.log(orderHistory)
+
+    if (!orderHistory) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'No orders found' })
     }
-    return db.insert(order)
-  }
-
-  static async findOrderByUserId(userId) {
-    return db.find({ type: 'order', userId })
+    //TODO Ordna så vi får ut datumet på rätt sätt. Ska vi formatera om det som ligger eller lägga in det på nytt?
+    // const formatedDates = orderHistory.map(order => {
+    //   date: moment(order.delivery).format('YYYY-MM-DD'), order.price
+    // })
+    // // console.log(orderHistory)
+    return orderHistory
+  } catch (error) {
+    console.error('Failed to retrieve order history', error)
+    throw new Error('Failed to retrieve order history')
   }
 }
 
-module.exports = Order
+module.exports = { GetOrderByID, CreateOrder, getOrderHistory }
