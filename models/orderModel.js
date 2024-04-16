67d032a7c9df3ede2f0f4a7a6e081bcf72538dbf
Mattias Rensmo <mattias@rensmo.se>
2024-04-16 15:21:52 +0200

    kul med ändringar

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,23 +1,21 @@
 const Datastore = require('nedb-promise')
 const db = new Datastore({ filename: './database/database.db', autoload: true })
 
-
 class Order {
-
-    static async createOrder(orderDetails) {
-
-        const order = { // schema
-            type: 'order',
-            price: orderDetails.price,
-            customer: orderDetails.items,
-            items: orderDetails.items
-        }
-        return db.insert(order)
+  static async createOrder(orderDetails) {
+    const order = {
+      // schema
+      type: 'order',
+      price: orderDetails.price,
+      customer: orderDetails.items,
+      items: orderDetails.items,
     }
+    return db.insert(order)
+  }
 
-    static async findOrderByUserId(userId) {
-        return db.find({ type: 'order', userId })
-    }
+  static async findOrderByUserId(userId) {
+    return db.find({ type: 'order', userId })
+  }
 }
 
 module.exports = Order
