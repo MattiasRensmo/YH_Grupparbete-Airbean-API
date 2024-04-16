8c45f6c2b1087a78f9ffafbe97c591402eddbe71
Mattias Rensmo <mattias@rensmo.se>
2024-04-16 15:26:14 +0200

    kul

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,5 @@
 const Order = require('../models/OrderModel')
 
-// exports.placeOrder = async (req, res) => {
-
-//     // Från frontend...
-//     // const orderData = {
-//     //     userID: "123ABC"
-//     //     products: [
-//     //         {productID: 1, quantity: 1, price: 49},
-//     //         {productID: 2, quantity: 2, price: 49}
-//     //     ],
-//     // }
-
-//     try {
-//         const orderData = req.body
-//         const insertOrder = await Order.placeOrder(orderData)
-//         return res.status(201).json({success: true, order: insertOrder})
-//     } catch (error) {
-//         console.error("Error when placing an order", error)
-//         return res.status(500).json({success: false, message: "Failed to place en order"})
-//     }
-// }
-
 exports.getOrderHistory = async (req, res) => {
   try {
     const userID = req.params.userID
