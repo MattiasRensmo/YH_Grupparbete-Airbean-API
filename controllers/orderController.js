73bb5d60dc0e95ab378ef4658e0a5b167c3a77ba
J0113boy <keskitalo_6@hotmail.com>
2024-04-17 11:44:03 +0200

    random sista förändringar

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,16 +1,20 @@
 const Order = require('../models/orderModel')
 
+
+
 exports.placeOrder = async (req, res) => {
     try {
         const orderDetails = req.body
         orderDetails.userId = req.user._id // req.user bör ju sättas efter att användaren har loggats in 
-        const order = await Order.createOrder(orderDetails) // vi skickar orderDetails som argument och väntar resultatet utav createOrder
+        const order = await Order.createOrder(orderDetails) // vi skickar orderDetails som argument och inväntar resultatet utav createOrder, vars uträkning uförs i vår orderModel
         res.status(200).send(order) // och skickar sedan ordern som ett respons-objekt 
     
     } catch (error) {
         res.status(500).send({ error: 'Internal server error'})
     }
 }
+
+
 
 exports.getOrderHistory = async (req, res) => {
     try {
